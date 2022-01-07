49fdd558a3e72745b3066011b67357e904aa4ad1
Apoorv Jain <apoorv.jain.ece19@itbhu.ac.in>
2022-01-07 14:11:00 +0530

    chat-engine

---
+++ index.js
@@ -0,0 +1,53 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
+const passportjwt = require("./config/passport-jwt-strategy");
+const app = express();
+const Like = require("./models/like");
+const Post = require("./models/post");
+const Comment = require("./models/comment");
+const Friendship = require("./models/friendship");
+const User = require("./models/user");
+const Conversation = require("./models/conversation");
+const Message = require("./models/message");
+
+const db = require("./config/mongoose");
+
+let socket_port = process.env.SOCKET;
+if (socket_port == "" || socket_port == null) {
+  socket_port = 8000;
+}
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "https://powerful-hamlet-85569.herokuapp.com",
+    ],
+    methods: ["GET", "POST"],
+  },
+}).listen(socket_port);
+
+io.on("connection", function (socket) {
+  console.log(socket.id);
+  socket.on("join_room", (room) => {
+    socket.join(room);
+  });
+  socket.on("send-message", async function (message, roomid) {
+    console.log(message, roomid);
+    let conv = await Conversation.findOne({ friendship: roomid });
+    let newmsg = await Message.create({
+      user: message.user._id,
+      content: message.content,
+    });
+    conv.messages.push(newmsg);
+    conv.save();
+    io.to(roomid).emit("receive-message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+
+const AVATAR_PATH = path.join("/uploads/users/avatars");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Friendship",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let fpath = path.join(__dirname, "..", AVATAR_PATH).replace(/\\/g, "/");
+    fpath = path.normalize(fpath);
+    cb(null, fpath);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+//static functions for all users
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+  "avatar"
+);
+userSchema.statics.avatarPath = AVATAR_PATH;
+console.log("PATH", userSchema.statics.avatarPath);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
