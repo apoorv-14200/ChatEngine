dc564a62d8c5ca6646ee91fa085792d5665b5894
Apoorv Jain <apoorv.jain.ece19@itbhu.ac.in>
2022-01-07 14:44:41 +0530

    final

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ if (socket_port == "" || socket_port == null) {
   socket_port = 8000;
 }
 
+app.use(cors());
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
