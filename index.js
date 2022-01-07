0637dc331dc9c6b85079a0bc9c3fc184cbd8ae06
Apoorv Jain <apoorv.jain.ece19@itbhu.ac.in>
2022-01-07 14:38:00 +0530

    final

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ if (socket_port == "" || socket_port == null) {
   socket_port = 8000;
 }
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
